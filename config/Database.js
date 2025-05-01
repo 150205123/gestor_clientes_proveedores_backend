82597d0222a4769c1610d20d631e033221714862
DanteLuque <danteluque706@gmail.com>
2025-05-01 10:04:22 -0500

    creando nuevas variables de entorno

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize('VVDB','root','Coral1502',
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
-        dialect: "mysql"
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
     }
 );
+
+db.authenticate()
+    .then(() => console.log('Conexión a la base de datos exitosa'))
+    .catch(err => console.error('Error al conectar a la base de datos:', err));
 
 export default db;
